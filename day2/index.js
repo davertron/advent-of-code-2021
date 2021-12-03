aee56dddda639d8e5b8edfd4d7f08fac0d4654b5
Dave Davis <dabukun@gmail.com>
2021-12-03 09:19:18 -0500

    Day 2 part 2

--- day2/index.js
+++ day2/index.js
@@ -33,4 +33,37 @@ function part1() {
   return result;
 }
 
+function part2() {
+  let result = 0;
+  let horizontal = 0;
+  let depth = 0;
+  let aim = 0;
+
+  lines.forEach((line) => {
+    const matcher = /(forward|down|up) (\d+)/;
+    const matches = matcher.exec(line);
+    if (matches) {
+      let [_, direction, amount] = matches;
+      amount = parseInt(amount, 10);
+
+      if (direction === "forward") {
+        horizontal += amount;
+        depth += aim * amount;
+      } else if (direction === "up") {
+        aim -= amount;
+      } else if (direction === "down") {
+        aim += amount;
+      } else {
+        throw new Error("Bad direction:", direction);
+      }
+    }
+  });
+
+  result = horizontal * depth;
+
+  console.log("Part 2:", result);
+  return result;
+}
+
 part1();
+part2();
