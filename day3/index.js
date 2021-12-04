42328a56bd071dcec24c2de4d5a766f64923a01f
Dave Davis <dabukun@gmail.com>
2021-12-04 15:35:21 -0500

    Day 3 part 2

--- day3/index.js
+++ day3/index.js
@@ -35,4 +35,53 @@ function part1() {
   return gamma * epsilon;
 }
 
+// Part 2
+
+function getLeastCommonBitAtIndex(values, index) {
+  let ones = 0;
+  values.forEach((v) => {
+    if (v[index] === "1") ones++;
+  });
+
+  return ones >= values.length / 2 ? "0" : "1";
+}
+
+function getMostCommonBitAtIndex(values, index) {
+  let ones = 0;
+  values.forEach((v) => {
+    if (v[index] === "1") ones++;
+  });
+
+  return ones >= values.length / 2 ? "1" : "0";
+}
+
+function getOxygenGeneratorRating(readings) {
+  let results = [...readings];
+  let currentBitIndex = 0;
+  while (results.length > 1) {
+    let mostCommonBit = getMostCommonBitAtIndex(results, currentBitIndex);
+    results = results.filter((r) => r[currentBitIndex] === mostCommonBit);
+    currentBitIndex++;
+  }
+  return parseInt(results[0], 2);
+}
+
+function getCO2ScrubberRating(readings) {
+  let results = [...readings];
+  let currentBitIndex = 0;
+  while (results.length > 1) {
+    let leastCommonBit = getLeastCommonBitAtIndex(results, currentBitIndex);
+    results = results.filter((r) => r[currentBitIndex] === leastCommonBit);
+    currentBitIndex++;
+  }
+  return parseInt(results[0], 2);
+}
+
+function part2() {
+  const oxygenGeneratorRating = getOxygenGeneratorRating(lines);
+  const co2ScrubberRating = getCO2ScrubberRating(lines);
+  console.log("Part 2: ", oxygenGeneratorRating * co2ScrubberRating);
+}
+
 part1();
+part2();
