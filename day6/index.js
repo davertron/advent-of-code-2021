d5ee310caad92cc5eeada6f269451fd4d5bb45be
Dave Davis <dabukun@gmail.com>
2021-12-08 20:52:30 -0500

    Day 6 part 2

--- day6/index.js
+++ day6/index.js
@@ -9,38 +9,46 @@ function getInput(filename) {
 }
 
 function simulateFish(fish, days) {
+    let fishCounts = new Array(9).fill(0);
+    for (let f of fish) {
+        fishCounts[f]++;
+    }
     for (let i = 0; i < days; i++) {
-        let fishToAdd = 0;
-        fish = fish.map((f) => {
-            if (f === 0) {
-                fishToAdd++;
-                return 6;
-            } else {
-                return f - 1;
-            }
-        });
-        for (let j = 0; j < fishToAdd; j++) {
-            fish.push(8);
-        }
+        let nextFishCounts = new Array(9).fill(0);
+        // All zeroes spawn an 8
+        nextFishCounts[8] = fishCounts[0];
+        // All 8's become 7's
+        nextFishCounts[7] = fishCounts[8];
+        // All 7's and 0's become 6's
+        nextFishCounts[6] = fishCounts[7] + fishCounts[0];
+        // Everything else just shifts down one...
+        nextFishCounts[5] = fishCounts[6];
+        nextFishCounts[4] = fishCounts[5];
+        nextFishCounts[3] = fishCounts[4];
+        nextFishCounts[2] = fishCounts[3];
+        nextFishCounts[1] = fishCounts[2];
+        nextFishCounts[0] = fishCounts[1];
+
+        fishCounts = nextFishCounts;
     }
 
-    return fish;
+    return fishCounts;
 }
 
 function part1() {
     const lines = getInput("input.txt");
     let fish = lines[0].split(",").map((v) => parseInt(v));
-    const days = 80;
-    fish = simulateFish(fish, days);
-    console.log("Part 1: ", fish.length);
+    const counts = simulateFish(fish, 80);
+    const total = counts.reduce((acc, count) => acc + count);
+    console.log("Part 1: ", total);
 }
 
 function part2() {
-    const lines = getInput("sample.txt");
+    const lines = getInput("input.txt");
     let fish = lines[0].split(",").map((v) => parseInt(v));
-    const days = 256;
-    //fish = simulateFish(fish, days);
-    console.log("Part 2: ", fish.length);
+    const counts = simulateFish(fish, 256);
+    const total = counts.reduce((acc, count) => acc + count);
+    console.log("Part 2: ", total);
 }
 
 part1();
